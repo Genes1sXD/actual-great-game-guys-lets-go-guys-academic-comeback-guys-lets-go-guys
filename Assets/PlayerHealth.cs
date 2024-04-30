fd820425f6dee0803d03548f7f1749d99fcd3066
W1LLUMP <silverspelaren@gmail.com>
2024-04-30 12:11:55 +0200

    Revert "Merge branch 'main2' of https://github.com/Genes1sXD/actual-great-game-guys-lets-go-guys-academic-comeback-guys-lets-go-guys into main2"
    This reverts commit b522ac1e929d61683b1ebe53ce3ce769196a664d, reversing
    changes made to e1c34550c76441650cc312be94db307cac89f9af.

--- Assets/PlayerHealth.cs
+++ Assets/PlayerHealth.cs
@@ -7,8 +7,6 @@ public class PlayerHealth : MonoBehaviour
     public int maxHealth = 100;
     public int currentHealth;
     public bool isInvulnerable = false;
-    public bool isAlive = true;
-  
 
     public event System.Action<int> OnHealthChanged;
     public event System.Action OnPlayerDied;
@@ -49,11 +47,10 @@ public class PlayerHealth : MonoBehaviour
         OnHealthChanged?.Invoke(currentHealth);
     }
 
-    public void Die()
+    private void Die()
     {
         OnPlayerDied?.Invoke();
         Debug.Log("Player Died!");
-        isAlive = false; //spelaren dör
         // Disable player controls or trigger death animation
         // Consider disabling the script or the component that handles player input
     }
